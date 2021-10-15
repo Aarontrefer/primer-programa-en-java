d53435512fe714ccfa7580411cca82aae4db78e5
Aaron Treceño <aarontreceno10@gmail.com>
2021-10-15 12:28:24 +0200

    Mi cantante Favorito

--- Prueba.java
+++ Prueba.java
@@ -2,10 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Aaron Treceño Fernandez");
 		System.out.println("Naci en Palencia en el Año 2001");
 		System.out.println("Resido en Leon y tengo un Windows 10 Pro Education");
-
+		System.out.println("Mi cantante favorito es Myke Towers");
 	}
 
 
